0d7c6777343c9ed6c3482e0433f869cd3bd968a3
leen165 <leen_zien@web.de>
2024-06-08 01:57:26 +0200

    leen's update

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,6 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
