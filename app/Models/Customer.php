1f2b952c1c8a0aeb2fb45791de5da8fd06ad34b2
Aram Hag <aramaa707@gmail.com>
2024-06-23 12:09:27 +0300

    merge leen branch to main branch

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -8,13 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Customer extends Model
 {
     use HasFactory;
-<<<<<<< HEAD
     protected $fillable = [
         'name',
         'email',
         'phone',
         'address',
     ];
-=======
->>>>>>> 0d7c677 (leen's update)
 }
